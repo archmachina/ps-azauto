5318ee6b9e270469f51ec321737c524154fc9bd0
Jesse Reichman <jesse.reichman@noveris.com.au>
2022-03-27 21:59:30 +1030

    Fixed name refs. Remove docker bld.

--- Build.ps1
+++ Build.ps1
@@ -48,31 +48,12 @@ if ($version.FullVersion -ne "0.1.0")
     $dockerTags += $version.FullVersion
 }
 
-# Add additional tags, if not prerelease
-if (!$version.IsPrerelease -and $version.FullVersion -ne "0.1.0")
-{
-    $dockerTags += ("{0}" -f $version.Major)
-    $dockerTags += ("{0}.{1}" -f $version.Major, $version.Minor)
-    $dockerTags += ("{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Patch)
-}
-
-# Add any explicit docker tags
-if (![string]::IsNullOrEmpty($Env:DOCKER_TAGS))
-{
-    $Env:DOCKER_TAGS.split(";") | ForEach-Object { $dockerTags += $_}
-}
-
-Write-Information "Docker Tags:"
-$dockerTags | ConvertTo-Json
-
-$dockerImageName = "noverisinf/certcheck"
-
 ########
 # Build stage
 Invoke-BuildStage -Name "Build" -Filters $Stages -Script {
     # Template PowerShell module definition
-    Write-Information "Templating Noveris.CertCheck.psd1"
-    Format-TemplateFile -Template source/Noveris.CertCheck.psd1.tpl -Target source/Noveris.CertCheck/Noveris.CertCheck.psd1 -Content @{
+    Write-Information "Templating Noveris.AzAuto.psd1"
+    Format-TemplateFile -Template source/Noveris.AzAuto.psd1.tpl -Target source/Noveris.AzAuto/Noveris.AzAuto.psd1 -Content @{
         __FULLVERSION__ = $version.PlainVersion
     }
 
@@ -82,25 +63,20 @@ Invoke-BuildStage -Name "Build" -Filters $Stages -Script {
 
     # Install any dependencies for the module manifest
     Write-Information "Installing required dependencies from manifest"
-    Install-PSModuleFromManifest -ManifestPath source/Noveris.CertCheck/Noveris.CertCheck.psd1
+    Install-PSModuleFromManifest -ManifestPath source/Noveris.AzAuto/Noveris.AzAuto.psd1
 
     # Test the module manifest
     Write-Information "Testing module manifest"
-    Test-ModuleManifest source/Noveris.CertCheck/Noveris.CertCheck.psd1
+    Test-ModuleManifest source/Noveris.AzAuto/Noveris.AzAuto.psd1
 
     # Import modules as test
     Write-Information "Importing module"
-    Import-Module ./source/Noveris.CertCheck/Noveris.CertCheck.psm1
-
-    # Docker build
-    Write-Information ("Building for {0}" -f $dockerImageName)
-    & { $ErrorActionPreference="Continue" ; docker build -f ./source/Dockerfile -q -t $dockerImageName ./source } *>&1 | Out-String -Stream
-    Assert-SuccessExitCode $LASTEXITCODE
+    Import-Module ./source/Noveris.AzAuto/Noveris.AzAuto.psm1
 }
 
 Invoke-BuildStage -Name "Release" -Filters $Stages -Script {
     $owner = "noveris-inf"
-    $repo = "ps-certcheck"
+    $repo = "noveris-ps-azauto"
 
     $releaseParams = @{
         Owner = $owner
@@ -123,29 +99,5 @@ Invoke-BuildStage -Name "Release" -Filters $Stages -Script {
 Invoke-BuildStage -Name "Publish" -Filters $Stages -Script {
     # Publish module
     Write-Information "Publishing module"
-    Publish-Module -Path ./source/Noveris.CertCheck -NuGetApiKey $Env:NUGET_API_KEY
-}
-
-Invoke-BuildStage -Name "Push" -Filters $Stages -Script {
-    # Attempt login to docker registry
-    Write-Information "Attempting login for docker registry"
-    & { $ErrorActionPreference="Continue" ; $Env:DOCKER_TOKEN | docker login --password-stdin -u $Env:DOCKER_USERNAME $Env:DOCKER_REGISTRY } *>&1 | Out-String -Stream
-    Assert-SuccessExitCode $LASTEXITCODE
-
-    # Push docker images
-    Write-Information "Pushing docker tags"
-    $dockerTags | ForEach-Object {
-        $tag = $_
-        $path = ("{0}:{1}" -f $dockerImageName, $_)
-
-        # Docker tag
-        Write-Information ("Tagging build for {0}" -f $tag)
-        & { $ErrorActionPreference="Continue" ; docker tag $dockerImageName $path } *>&1 | Out-String -Stream
-        Assert-SuccessExitCode $LASTEXITCODE
-
-        # Docker push
-        Write-Information ("Docker push for for {0}" -f $path)
-        & { $ErrorActionPreference="Continue" ; docker push $path } *>&1 | Out-String -Stream
-        Assert-SuccessExitCode $LASTEXITCODE
-    }
+    Publish-Module -Path ./source/Noveris.AzAuto -NuGetApiKey $Env:NUGET_API_KEY
 }
